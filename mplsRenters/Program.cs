b24ddc3ad98d254415a2d8db3c9555a73c7b4046
fulchr <cfulton@dunwoody.edu>
2019-02-14 12:29:04 -0600

    adding functionality to read and write from csv file

--- mplsRenters/Program.cs
+++ mplsRenters/Program.cs
@@ -10,6 +10,8 @@ namespace mplsRenters
         {
             string name, address, regionOfCity;
             int floorCount;
+            string[] arrayOfValues;
+            List<Apartment> listOfApartments = new List<Apartment>();
 
             //Apartment aApartment = new Apartment(); //Instantiation -Create new instance from the Apartment Class
             //aApartment.Name = "Nord Haus";
@@ -34,7 +36,80 @@ namespace mplsRenters
             string adjustedFilePath = $@"{stepBackThree}\apartment_list.csv";
             Console.WriteLine(adjustedFilePath);
 
+            if (File.Exists(adjustedFilePath))
+            {
 
+                FileStream file = new FileStream(adjustedFilePath, FileMode.Open, FileAccess.Read);
+                StreamReader read = new StreamReader(file);
+
+                while (!read.EndOfStream)
+                {
+                    try
+                    {
+                        //Serialize the objects into our program
+                        arrayOfValues = read.ReadLine().Split(',');
+                        listOfApartments.Add(new Apartment(arrayOfValues[0], arrayOfValues[1], Int32.Parse(arrayOfValues[2]), arrayOfValues[3]));
+                    }catch(Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                       
+                    }
+
+                }
+                read.Close();
+                file.Close();
+
+                listOfApartments.ForEach(i => Console.WriteLine(i.ToString()));
+
+
+                //Demonstrate Writing to a file
+                FileStream aFile = new FileStream(adjustedFilePath, FileMode.Append, FileAccess.Write);
+                StreamWriter writeInfo = new StreamWriter(aFile);
+
+
+                Apartment[] apartments = new Apartment[2];
+                var x = 0;
+                while (x < apartments.Length)
+                {
+
+                    Console.WriteLine("Please enter apartment name");
+                    name = Console.ReadLine();
+
+                    Console.WriteLine("Please enter address");
+                    address = Console.ReadLine();
+
+                    Console.WriteLine("Please enter floor count");
+                    floorCount = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Please enter Region of city");
+                    regionOfCity = Console.ReadLine();
+
+
+                    try
+                    {
+                        apartments[x] = new Apartment(name, address, floorCount, regionOfCity);
+                        writeInfo.WriteLine($"{name},{address},{floorCount},{regionOfCity}");
+
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        
+                        writeInfo.WriteLine($"{name},{address},{floorCount},None");
+
+                    }
+                    x++;
+
+                }//End of While
+
+                writeInfo.Close();
+                aFile.Close();
+            }
+            else
+            {
+                Console.WriteLine("The file does not exist, please identify the file and restart program");
+            }
 
 
         }
